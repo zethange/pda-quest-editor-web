1a7b5f8a4162ece9428a6147ab990f9f2d02e1a3
ZetHange <maksselles@gmail.com>
2023-02-20 17:35:54 +0300

    add upload function

--- components/UI/Card.tsx
+++ components/UI/Card.tsx
@@ -1,10 +1,40 @@
-export default function Card({ title, children }: { title: string, children: React.ReactNode }) {
+import React, { useEffect } from "react";
+import Link from "next/link";
+
+export default function Card({
+  title,
+  children,
+  link,
+  id,
+}: {
+  title: string;
+  children: React.ReactNode;
+  link: any;
+  id: any;
+}) {
+  const deleteChapter = () => {
+    console.log(`Удаляем стадию с ID: ${id}`);
+    localStorage.removeItem("chapter_" + id);
+  };
+
   return (
     <div className="card">
-      <div className="card__header">{title}</div>
-      <div className="card__body">
-        {children}
-      </div>
+      <Link href={link}>
+        <div className="card__header">{title}</div>
+        <div className="card__body">{children}</div>
+      </Link>
+      <button
+        style={{
+          fontSize: "15px",
+          padding: "10px",
+          position: "absolute",
+          top: "0px",
+          right: "0px",
+        }}
+        onClick={() => deleteChapter()}
+      >
+        Удалить
+      </button>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,48 @@
 import Head from "next/head";
-import NavBars from "@/components/NavBars";
-import Cards from "@/components/Cards";
+import Card from "@/components/UI/Card";
+import NavBar from "@/components/UI/NavBar";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [listChapters, setListChapters] = useState<any>([]);
+
+  const onChangeChapter = async (e: any) => {
+    const file = await e.target.files[0];
+    let reader = await new FileReader();
+    await reader.readAsText(file);
+
+    reader.onload = await function () {
+      if (typeof reader.result === "string") {
+        localStorage.setItem(
+          String("chapter_" + JSON.parse(reader.result).id),
+          reader.result
+        );
+      }
+    };
+
+    reader.onerror = await function () {
+      console.log(reader.error);
+    };
+  };
+
+  useEffect(() => {
+    const keyStages = [];
+    const stages: any[] = [];
+
+    for (let item in localStorage) {
+      if (item.split("_")[1] !== undefined) {
+        keyStages.push(item);
+      }
+    }
+
+    keyStages.map((stage) => {
+      // @ts-ignore
+      stages.push(JSON.parse(localStorage.getItem(stage)));
+    });
+
+    setListChapters(stages);
+  });
+
   return (
     <>
       <Head>
@@ -12,9 +52,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="main">
-        <NavBars />
+        <NavBar>
+          <p className="navbar__header navbar__header--active">История</p>
+          <p className="navbar__header">Карта</p>
+          <p className="navbar__header">Всего историй: {listChapters.length}</p>
+          <p className="mx-auto"></p>
+          <p className="navbar__header">Помощь</p>
+        </NavBar>
+        <hr />
+        <NavBar>
+          <input
+            type="file"
+            className="navbar__header"
+            onChange={onChangeChapter}
+            accept="application/json"
+          />
+          <p className="navbar__header">
+            <i className="fa-solid fa-pen-to-square"></i> Редактирование
+          </p>
+        </NavBar>
         <div className="work-space">
-          <Cards />
+          <div className="card-parent">
+            {listChapters.map((chapter: any) => (
+              <Card
+                title={"Глава " + chapter?.id}
+                link={"/edit/chapter/" + chapter?.id}
+                key={chapter?.id}
+                id={chapter?.id}
+              >
+                <div>Количество стадий: {chapter?.stages.length}</div>
+              </Card>
+            ))}
+          </div>
         </div>
       </main>
     </>
