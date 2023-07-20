b056a82cc522ade1fbc5c1f675394151fcaa13bc
Maxim <85435455+ZetHange@users.noreply.github.com>
2023-07-20 14:08:23 +0300

    fix: error resetParametersUpload

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,6 +81,7 @@ export default function Home() {
 
   const resetParametersUpload = () => {
     setParametersUpload({
+      id: 0,
       type: "PUBLIC",
       toStore: false,
       message: "",
