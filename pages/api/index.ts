07d93c85d46ec2064865aea79ce751595ed2fa68
Maxim <85435455+ZetHange@users.noreply.github.com>
2023-08-03 16:14:29 +0300

    delete: nextauth routes

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const runtime = "edge";
-
 export default async (res: NextApiResponse) => {
   res.status(200).json({
     error: "Понимаешь что такое API?",
